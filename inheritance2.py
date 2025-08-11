35f97d565b46edc8580a61ce89901f6458734263
nyagetiria <nyagetirianaomi@gmail.com>
2025-08-11 12:42:19 +0300

    inheritance2.py

--- inheritance2.py
+++ inheritance2.py
@@ -1,25 +1,67 @@
-class user():
-    
-    def __init__(self,name,email,phone,password,discount):
-        self.name = name
-        self.email = email
-        self.phone = phone
-        self.password = password
-        self.discount = discount
+
+
+#Ecomerce
+
+class User():
+
+    def __init__(self,name,email,phone,password,user,discount=0):
+        self.name=name
+        self.email=email
+        self.phone=phone
+        self.password=password
+        self.discount=discount
+        self.user=user
+        self.is_locked=False
 
     def say_my_name(self):
-        print(f"My name {self.name}.")
-
-
-
-class Employee(user):
+        print(f"My name {self.name}")
     
-    def __init__(self,name,email,phone,password,salary):
-        super().__init__(name,email,phone,password,discount)
-        self.salary = salary
+    def print_details(self):
+        print("-----------------")
+        print("User",self.user)
+        print("Name",self.name)
+        print("Email",self.email)
+        print("Phone",self.phone)
+        print("Discount",self.discount)
+        print("-----------------")
 
-class Customer(user):
+    def login(self):
 
-    def __init__(self,name,email,phone,password,discount):
-        super().__init__(name,email,phone,password,0)
-     
+        if self.is_locked:
+            print("Accont already locked")
+            return
+
+        for i in range(1,3):
+            password=input(f"Enter your password: Attempt {i}")  
+            if self.password==password:
+                print("Logged in success")
+                return
+        self.is_locked=True
+        print("Account locked contact admin")
+                      
+
+class Employee(User):
+    
+    def __init__(self, name, email, phone,password,salary):
+        super().__init__(name=name, email=email, phone=phone,password=password,discount=10,user="employee")
+        self.salary=salary
+
+class Customer(User):
+    
+    def __init__(self, name, email, phone,password):
+        super().__init__(name=name, email=email, phone=phone,password=password,discount=0,user="customer")
+        
+
+
+emp1=Employee(name="Sam",email="sam@sam.com",phone=1234234,
+              password=1234,salary=20000)
+
+c1=Customer(name="Delilah",email="del@gmail.com",phone=2343,password=1234)
+
+emp1.say_my_name()
+c1.say_my_name()
+
+emp1.print_details()
+c1.print_details()
+
+emp1.login()
